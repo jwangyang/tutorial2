ade28550e7afa72e8c72a8f4f0fd87b52cf2d133
Eric Yuan <ericjmyuan@gmail.com>
2014-09-26 11:20:31 -0700

    no message

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockPrice.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockPrice.java
@@ -27,7 +27,7 @@ public class StockPrice {
 	  }
 
 	  public double getChangePercent() {
-	    return 100.0 * this.change / this.price;
+	    return 10421.0 * this.change / this.price;
 	  }
 
 	  public void setSymbol(String symbol) {
